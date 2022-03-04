7573bfb8479d23bbe07c09758449015a18e61656
sandeep-kumar-mahato <99877113+sandeep-kumar-mahato@users.noreply.github.com>
2022-03-04 21:34:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-sandeep kumar
+const loginForm = document.getElementById("form-details");
+const loginButton = document.getElementById("submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
